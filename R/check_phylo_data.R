69ba6d5ff910fd603379fb547e1fb7bb001722ef
joshwlambert <j.w.l.lambert@rug.nl>
2022-02-03 18:32:57 +0100

    added initial check_phylo_data

---
+++ R/check_phylo_data.R
@@ -0,0 +1,15 @@
+# this function checks whether phylo (or phylo4 or phylo4d) object conforms
+# to the requirements of the DAISIEprep package. If TRUE is returned the data
+# is ready to be used, if FALSE is returned the data requires some
+# pre-processing before DAISIEprep can be used
+#' Title
+#'
+#' @param phylo
+#'
+#' @return
+#' @export
+#'
+#' @examples
+check_phylo_data <- function(phylo) {
+
+}
