254f6ea09862e4823e9f7b0173c3255a5c75af76
joshwlambert <j.w.l.lambert@rug.nl>
2022-02-11 13:14:21 +0100

    updated extract_endemic to handle multi-tip endemic species

--- R/extract_endemic.R
+++ R/extract_endemic.R
@@ -10,31 +10,49 @@
 #' @examples
 #' set.seed(1)
 #' phylo <- ape::rcoal(10)
-#' phylo <- as(phylo, "phylo4")
+#' phylo$tip.label <- c("bird_a", "bird_b", "bird_c", "bird_d", "bird_e",
+#'                      "bird_f", "bird_g", "bird_h", "bird_i", "bird_j")
+#' phylo <- methods::as(phylo, "phylo4")
 #' endemicity_status <- sample(c("not_present", "endemic", "nonendemic"),
-#'                               size = length(phylobase::tipLabels(phylo)),
-#'                               replace = TRUE)
+#'                             size = length(phylobase::tipLabels(phylo)),
+#'                             replace = TRUE)
 #' phylod <- phylobase::phylo4d(phylo, as.data.frame(endemicity_status))
-#' extract_endemic(phylod = phylod, species_label = "t1")
+#' island_col <- extract_endemic(
+#'   phylod = phylod,
+#'   species_label = "bird_c"
+#' )
 extract_endemic <- function(phylod,
                             species_label) {
 
   # create an instance of the island_colonist class to store data
   island_col <- island_colonist()
 
-  #TODO: write check that the species_label refers to nonendemic species
+  #TODO: write check that the species_label refers to endemic species
 
   # check whether the focal species is in an endemic clade
-  clade <- is_island_clade(phylod, species_label)
+  clade <- is_endemic_clade(phylod, species_label)
 
-  if (isFALSE(clade)) {
+  # does species have multiple tips in the tree (i.e. population sampling)
+  multi_tip_species <- is_multi_tip_species(
+    phylod = phylod,
+    species_label = species_label
+  )
+
+  singleton_endemic <- isFALSE(clade) && isFALSE(multi_tip_species)
+  multi_tip_endemic <- isFALSE(clade) && multi_tip_species
+  endemic_clade <- clade && isFALSE(multi_tip_species)
+  multi_tip_endemic_clade <- clade && multi_tip_species
+
+  if (singleton_endemic) {
     # assign data to instance of island_colonist class
     set_clade_name(island_col) <- species_label
     set_status(island_col) <- "endemic"
     set_missing_species(island_col) <- 0
     set_branching_times(island_col) <-
       as.numeric(phylobase::edgeLength(phylod, species_label))
-  } else {
+  } else if (multi_tip_endemic) {
+    island_col <- extract_multi_tip_endemic(phylod, species_label)
+  } else if (endemic_clade) {
     island_col <- extract_endemic_clade(phylod, species_label)
   }
 
