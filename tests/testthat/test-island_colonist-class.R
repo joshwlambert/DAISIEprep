4466ef71532c119557b8833c69ca0293d2b54b0c
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-04-03 18:20:21 +0200

    added island_colonist-class and island_tbl-class tests

--- tests/testthat/test-island_colonist-class.R
+++ tests/testthat/test-island_colonist-class.R
@@ -2,3 +2,32 @@ test_that("island_colonist class is correct", {
   island_colonist <- island_colonist()
   expect_s4_class(object = island_colonist, class = "Island_colonist")
 })
+
+test_that("check_island_colonist returns true when class is correct", {
+  island_colonist <- island_colonist()
+  expect_true(check_island_colonist(object = island_colonist))
+})
+
+test_that("check_island_colonist returns character vector when clade_name is not length 1", {
+  island_colonist <- island_colonist()
+  island_colonist@clade_name <- c("bird_a", "bird_b")
+  expect_vector(check_island_colonist(object = island_colonist))
+})
+
+test_that("check_island_colonist returns character vector when status is not length 1", {
+  island_colonist <- island_colonist()
+  island_colonist@status <- c("endemic", "endemic")
+  expect_vector(check_island_colonist(object = island_colonist))
+})
+
+test_that("check_island_colonist returns character vector when missing_species is not length 1", {
+  island_colonist <- island_colonist()
+  island_colonist@missing_species <- c(0, 0)
+  expect_vector(check_island_colonist(object = island_colonist))
+})
+
+test_that("check_island_colonist returns character vector when min_age is not length 1", {
+  island_colonist <- island_colonist()
+  island_colonist@min_age <- c(1, 1)
+  expect_vector(check_island_colonist(object = island_colonist))
+})
--- tests/testthat/test-island_tbl-class.R
+++ tests/testthat/test-island_tbl-class.R
@@ -2,3 +2,37 @@ test_that("island_tbl class is correct", {
   island_tbl <- island_tbl()
   expect_s4_class(object = island_tbl, class = "Island_tbl")
 })
+
+test_that("check_island_tbl returns true when class is correct", {
+  island_tbl <- island_tbl()
+  expect_true(check_island_tbl(object = island_tbl))
+})
+
+test_that("check_island_tbl returns character vector when class does not have 5 cols", {
+  island_tbl <- island_tbl()
+  island_tbl@island_tbl <- data.frame(
+    clade_name = character(),
+    status = character(),
+    missing_species = numeric(),
+    branching_times = numeric()
+  )
+  expect_vector(check_island_tbl(object = island_tbl))
+})
+
+test_that("check_island_tbl returns character vector when class does not have correct col names", {
+  island_tbl <- island_tbl()
+  island_tbl@island_tbl <- data.frame(
+    Clade_name = character(),
+    status = character(),
+    missing_species = numeric(),
+    branching_times = numeric(),
+    min_age = numeric()
+  )
+  expect_vector(check_island_tbl(object = island_tbl))
+})
+
+test_that("check_island_tbl returns character vector when class does not have correct metadata", {
+  island_tbl <- island_tbl()
+  island_tbl@metadata <- list()
+  expect_vector(check_island_tbl(object = island_tbl))
+})
