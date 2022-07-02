9124b5870f775000292542d2df083d43541b26a0
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-07-02 18:14:27 +0200

    added new slots to island_colonist class and methods

--- R/island_colonist-accessors.R
+++ R/island_colonist-accessors.R
@@ -70,6 +70,40 @@ setMethod("set_missing_species<-", "Island_colonist", function(x, value) {
 })
 
 #' @rdname Island_colonist-accessors
+setGeneric("get_col_time", function(x) standardGeneric("get_col_time"))
+
+#' @rdname Island_colonist-accessors
+#' @export
+setMethod("get_col_time", "Island_colonist", function(x) x@col_time)
+
+#' @rdname Island_colonist-accessors
+setGeneric("set_col_time<-", function(x, value) standardGeneric("set_col_time<-"))
+
+#' @rdname Island_colonist-accessors
+#' @export
+setMethod("set_col_time<-", "Island_colonist", function(x, value) {
+  x@col_time <- value
+  x
+})
+
+#' @rdname Island_colonist-accessors
+setGeneric("get_col_max_age", function(x) standardGeneric("get_col_max_age"))
+
+#' @rdname Island_colonist-accessors
+#' @export
+setMethod("get_col_max_age", "Island_colonist", function(x) x@col_max_age)
+
+#' @rdname Island_colonist-accessors
+setGeneric("set_col_max_age<-", function(x, value) standardGeneric("set_col_max_age<-"))
+
+#' @rdname Island_colonist-accessors
+#' @export
+setMethod("set_col_max_age<-", "Island_colonist", function(x, value) {
+  x@col_max_age <- value
+  x
+})
+
+#' @rdname Island_colonist-accessors
 setGeneric("get_branching_times", function(x) standardGeneric("get_branching_times"))
 
 #' @rdname Island_colonist-accessors
@@ -120,3 +154,19 @@ setMethod("set_species<-", "Island_colonist", function(x, value) {
   x
 })
 
+#' @rdname Island_colonist-accessors
+setGeneric("get_clade_type", function(x) standardGeneric("get_clade_type"))
+
+#' @rdname Island_colonist-accessors
+#' @export
+setMethod("get_clade_type", "Island_colonist", function(x) x@clade_type)
+
+#' @rdname Island_colonist-accessors
+setGeneric("set_clade_type<-", function(x, value) standardGeneric("set_clade_type<-"))
+
+#' @rdname Island_colonist-accessors
+#' @export
+setMethod("set_clade_type<-", "Island_colonist", function(x, value) {
+  x@clade_type <- value
+  x
+})
--- R/island_colonist-class.R
+++ R/island_colonist-class.R
@@ -32,10 +32,37 @@ check_island_colonist <- function(object) {
     errors <- c(errors, msg)
   }
 
+  length_col_time <- length(object@col_time)
+  if (length_col_time != 1) {
+    msg <- paste(
+      "col_time is length ", length_col_time, ". Should be 1",
+      sep = ""
+    )
+    errors <- c(errors, msg)
+  }
+
+  length_col_max_age <- length(object@col_max_age)
+  if (length_col_max_age != 1) {
+    msg <- paste(
+      "col_max_age is length ", length_col_max_age, ". Should be 1",
+      sep = ""
+    )
+    errors <- c(errors, msg)
+  }
+
   length_min_age <- length(object@min_age)
   if (length_min_age != 1) {
     msg <- paste(
       "min_age is length ", length_min_age, ". Should be 1",
+      sep = ""
+    )
+    errors <- c(errors, msg)
+  }
+
+  length_clade_type <- length(object@clade_type)
+  if (length_clade_type != 1) {
+    msg <- paste(
+      "clade_type is length ", length_clade_type, ". Should be 1",
       sep = ""
     )
     errors <- c(errors, msg)
@@ -55,8 +82,12 @@ check_island_colonist <- function(object) {
 #' @slot clade_name character.
 #' @slot status character.
 #' @slot missing_species character.
+#' @slot col_time numeric.
+#' @slot col_max_age logical.
 #' @slot branching_times numeric.
 #' @slot min_age numeric.
+#' @slot species character.
+#' @slot clade_type numeric.
 #'
 #' @export
 setClass(
@@ -68,9 +99,12 @@ setClass(
     clade_name = "character",
     status = "character",
     missing_species = "numeric",
+    col_time = "numeric",
+    col_max_age = "logical",
     branching_times = "numeric",
     min_age = "numeric",
-    species = "character"
+    species = "character",
+    clade_type = "numeric"
   ),
 
   # define the default values of the slots
@@ -78,9 +112,12 @@ setClass(
     clade_name = NA_character_,
     status = NA_character_,
     missing_species = NA_real_,
+    col_time = NA_real_,
+    col_max_age = NA,
     branching_times = NA_real_,
     min_age = NA_real_,
-    species = NA_character_
+    species = NA_character_,
+    clade_type = NA_integer_
   ),
 
   # check validity of class
@@ -103,21 +140,33 @@ setClass(
 #'   clade_name = "bird",
 #'   status = "endemic",
 #'   missing_species = 0,
-#'   branching_times = 0.5
+#'   col_time = 0.5,
+#'   col_max_age = FALSE,
+#'   branching_times = 0.5,
+#'   min_age = NA,
+#'   species = "bird_a",
+#'   clade_type = 1
 #')
 island_colonist <- function(clade_name = NA_character_,
                             status = NA_character_,
                             missing_species = NA_real_,
+                            col_time = NA_real_,
+                            col_max_age = NA,
                             branching_times = NA_real_,
                             min_age = NA_real_,
-                            species = NA_character_) {
+                            species = NA_character_,
+                            clade_type = NA_integer_) {
+
   methods::new(
     "Island_colonist",
     clade_name = clade_name,
     status = status,
     missing_species = missing_species,
+    col_time = col_time,
+    col_max_age = col_max_age,
     branching_times = branching_times,
     min_age = min_age,
-    species = species
+    species = species,
+    clade_type = clade_type
   )
 }
