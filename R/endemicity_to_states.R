150ebbab49683061fdf3fbfa3e5c07bdc2e98690
Theo Pannetier <t.s.c.pannetier@rug.nl>
2022-07-08 16:13:32 +0200

    funcs to extract ASRs from biogeobears output

--- R/endemicity_to_states.R
+++ R/endemicity_to_states.R
@@ -1,3 +1,10 @@
+#' All possible endemicity statuses
+#'
+#' @export
+all_endemicity_status <- function() {
+  return(c("not_present", "nonendemic", "endemic"))
+}
+
 #' Extract tip states from a phylod object
 #'
 #' @inheritParams default_params_doc
@@ -36,13 +43,6 @@ endemicity_to_states <- function(endemicity_status) {
   return(tip_states)
 }
 
-#' All possible endemicity statuses
-#'
-#' @export
-all_endemicity_status <- function() {
-  return(c("not_present", "nonendemic", "endemic"))
-}
-
 #' Convert SSE states back to endemicity status
 #'
 #' @param states integer vector of tip states, as expected by SSE models
@@ -57,9 +57,43 @@ states_to_endemicity <- function(states) {
   for (i in 1:3) {
     states <- gsub(
       pattern = as.character(i),
-      replacement = all_endemicity_status[i],
+      replacement = all_endemicity_status()[i],
       x = states
     )
   }
   return(states)
+}
+
+
+#' Select endemicity status from ancestral states probabilities
+#'
+#' Selects a state for each node (both internal nodes, i.e. ancestral states,
+#' and tips, if included) from a table of probabilities.
+#'
+#' @param asr_df a data frame containing at least these three columns:
+#' not_present_prob | endemic_prob | nonendemic_prob (in any order). Each column
+#' should contain the estimated probability of the state for each node (rows)
+#' and these columns should sum to 1.
+#' @param method "max" or "random". "max" will select the state with highest
+#' probability (selecting last state in event of a tie), while "random" will
+#' sample the states randomly with the probabilities as weight for each state.
+#'
+#' @return a character vector, with the selected endemicity status for each node.
+#' @export
+select_endemicity_status <- function(asr_df, method = "max") {
+  col_names <- paste0(all_endemicity_status(), "_prob")
+  asr_df <- asr_df[, col_names]
+  if (method == "max") {
+    asr_states <- max.col(asr_df, ties.method = "last")
+  } else if (method == "random") {
+    asr_states <- c()
+    for (i in 1:nrow(asr_df)) {
+      state <- sample(x = 1:3, size = 1, prob = asr_df[i, ])
+      asr_states[i] <- state
+    }
+  } else {
+    stop("Argument method should only be \"max\" or \"random\"")
+  }
+  endemicity_status <- states_to_endemicity(asr_states)
+  return(endemicity_status)
 }
--- R/write_biogeobears_input.R
+++ R/write_biogeobears_input.R
@@ -34,7 +34,7 @@ write_phylip_biogeo_file <- function(phylod, path_to_biogeo) {
 #' @export
 #'
 write_newick_file <- function(phylod, path_to_phylo) {
-  phylo <- as(phylod, "phylo")
+  phylo <- phylobase::as(phylod, "phylo")
   ape::write.tree(phylo, file = path_to_phylo)
 }
 
@@ -52,4 +52,33 @@ write_newick_file <- function(phylod, path_to_phylo) {
 write_biogeobears_input <- function(phylod, path_to_phylo, path_to_biogeo) {
   write_newick_file(phylod, path_to_phylo)
   write_phylip_biogeo_file(phylod, path_to_biogeo)
+}
+
+#' Extract ancestral state probabilities from BioGeoBEARS output
+#'
+#' Extract the probabilities of each endemicity status for tip and internal node
+#' states from the output of an optimisation performed with BioGeoBEARS
+#'
+#' @param biogeobears_res a list, the output of [BioGeoBEARS::bears_optim_run()]
+#' @return a data.frame with one row per node (tips and internals) and four
+#' columns: label | not_present | endemic | nonendemic, the last three columns
+#' containing the probability of each endemicity status (and summing to 1).
+#'
+#' @export
+#'
+extract_biogeobears_ancestral_states_probs <- function(biogeobears_res) {
+  # Extract probabilities
+  asr_likelihoods <- biogeobears_res$ML_marginal_prob_each_state_at_branch_top_AT_node[,-1]
+  # Need to find match tip labels to each row
+  tree <- ape::read.tree(biogeobears_res$inputs$trfn)
+  tip_labels <- tree$tip.label
+  node_labels <- tree$node.label
+  if (is.null(node_labels)) node_labels <- rep(NA, length(tip_labels) - 1)
+  asr_df <- data.frame(
+    label = c(tip_labels, node_labels),
+    not_present_prob = asr_likelihoods[,1],
+    endemic_prob = asr_likelihoods[,2],
+    nonendemic_prob = asr_likelihoods[,3]
+  )
+  return(asr_df)
 }
