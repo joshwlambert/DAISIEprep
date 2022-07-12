79c2068c7ccb49630e48be30b7944bf7ab793491
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-07-12 13:15:34 +0200

    updated plot_performance and read_performance and updated documentation

--- R/plot_performance.R
+++ R/plot_performance.R
@@ -1,239 +1,105 @@
-plot_performance <- function() {
+#' Plots performance results for a grouping variable (prob_on_island or
+#' prob_endemic).
+#'
+#' @param performance_data Tibble of collated performance results
+#' @param group_by A variable to partition by for plotting. Uses data masking
+#' so variable does not need to be quoted.
+#'
+#' @return ggplot2 object
+#' @export
+#'
+#' @examples
+#' performance_data <- read_performance()
+#' plot_performance(
+#'   performance_data = performance_data$performance_data_asr,
+#'   group_by = prob_on_island
+#' )
+#' plot_performance(
+#'   performance_data = performance_data$performance_data_min,
+#'   group_by = prob_endemic
+#' )
+plot_performance <- function(performance_data,
+                             group_by) {
 
-  # # tidy empirical data
-  #
-  # mean_times_min_dna <- performance_empirical$mean_times_min_dna
-  # mean_times_min_complete <- performance_empirical$mean_times_min_complete
-  # mean_times_asr_dna <- performance_empirical$mean_times_asr_dna
-  # mean_times_asr_complete <- performance_empirical$mean_times_asr_complete
-  #
-  # extraction_method <- c(
-  #   rep("min", length(mean_times_min_dna)),
-  #   rep("min", length(mean_times_min_complete)),
-  #   rep("asr", length(mean_times_asr_dna)),
-  #   rep("asr", length(mean_times_asr_complete))
-  # )
-  #
-  # tree_type <- c(
-  #   rep("dna", length(mean_times_min_dna)),
-  #   rep("complete", length(mean_times_min_complete)),
-  #   rep("dna", length(mean_times_asr_dna)),
-  #   rep("complete", length(mean_times_asr_complete))
-  # )
-  #
-  # tree_size <- c(
-  #   rep(4099, length(mean_times_min_dna)),
-  #   rep(5911, length(mean_times_min_complete)),
-  #   rep(4099, length(mean_times_asr_dna)),
-  #   rep(5911, length(mean_times_asr_complete))
-  # )
-  #
-  # mean_times <- c(
-  #   mean_times_min_dna,
-  #   mean_times_min_complete,
-  #   mean_times_asr_dna,
-  #   mean_times_asr_complete
-  # )
-  #
-  # empirical_results <- tibble::tibble(
-  #   mean_times = mean_times,
-  #   extraction_method = extraction_method,
-  #   tree_type = tree_type,
-  #   tree_size = tree_size
-  # )
-  #
-  # grouped_empirical_results <- dplyr::group_by(
-  #   empirical_results,
-  #   tree_size,
-  #   tree_type
-  # )
-  #
-  # mean_empirical_results <- dplyr::summarise(
-  #   grouped_empirical_results,
-  #   mean = mean(mean_times),
-  #   .groups = "drop"
-  # )
-  # sd_empirical_results <- dplyr::summarise(
-  #   grouped_empirical_results,
-  #   sd = sd(mean_times),
-  #   .groups = "drop"
-  # )
-  #
-  # summary_empirical_results <- dplyr::right_join(
-  #   mean_empirical_results,
-  #   sd_empirical_results,
-  #   by = c("tree_size", "tree_type")
-  # )
-  #
-  # min <- lapply(performance_data, "[[", "min")
-  #
-  # asr <- lapply(performance_data, "[[", "asr")
-  #
-  # parameter <- lapply(performance_data, "[[", "parameter_index")
-  #
-  # parameter_space <- expand.grid(
-  #   tree_size = c(10, 50, 100, 500, 1000, 5000, 10000),
-  #   prob_on_island = c(0.2, 0.5),
-  #   prob_endemic = c(0.2, 0.8)
-  # )
-  #
-  # parameter <- lapply(parameter, function(x) {
-  #   parameter_space[x, ]
-  # })
-  #
-  # tree_size <- sapply(parameter, "[[", "tree_size")
-  # prob_on_island <- sapply(parameter, "[[", "prob_on_island")
-  # prob_endemic <- sapply(parameter, "[[", "prob_endemic")
-  #
-  # results <- tibble::tibble(
-  #   tree_size = rep(tree_size, each = 10),
-  #   prob_on_island = rep(prob_on_island, each = 10),
-  #   prob_endemic = rep(prob_endemic, each = 10),
-  #   time_min = unlist(min),
-  #   time_asr = unlist(asr)
-  # )
-  #
-  # results <- tidyr::pivot_longer(
-  #   data = results,
-  #   names_to = "extraction",
-  #   time_min:time_asr,
-  #   values_to = "time"
-  # )
-  #
-  # grouped_results <- dplyr::group_by(results, tree_size, prob_on_island)
-  # mean_results <- dplyr::summarise(
-  #   grouped_results,
-  #   mean = mean(time),
-  #   .groups = "drop"
-  # )
-  # sd_results <- dplyr::summarise(
-  #   grouped_results,
-  #   sd = sd(time),
-  #   .groups = "drop"
-  # )
-  #
-  # summary_results <- dplyr::right_join(
-  #   mean_results,
-  #   sd_results,
-  #   by = c("tree_size", "prob_on_island")
-  # )
-  #
-  # summary_results$prob_on_island <- as.factor(summary_results$prob_on_island)
-  #
-  # performance_prob_on_island <- ggplot2::ggplot(data = summary_results) +
-  #   ggplot2::geom_point(
-  #     mapping = ggplot2::aes(
-  #       x = tree_size,
-  #       y = mean,
-  #       colour = prob_on_island
-  #     ),
-  #     position = ggplot2::position_dodge(width = 0.5),
-  #     size = 2,
-  #   ) +
-  #   ggplot2::geom_errorbar(
-  #     mapping = ggplot2::aes(
-  #       x = tree_size,
-  #       y = mean,
-  #       ymin = pmax(0.01, mean-sd),
-  #       ymax = mean+sd,
-  #       colour = prob_on_island
-  #     ),
-  #     position = ggplot2::position_dodge(width = 0.5),
-  #     width = 0.3
-  #   ) +
-  #   ggplot2::theme_classic() +
-  #   ggplot2::scale_x_continuous(
-  #     name = "Tree Size (num. tips)",
-  #     trans = "log",
-  #     breaks = scales::breaks_log()
-  #   ) +
-  #   ggplot2::scale_y_continuous(
-  #     name = "Mean run time (seconds)",
-  #     trans = "log",
-  #     breaks = scales::breaks_log(),
-  #     labels = scales::comma_format(accuracy = 0.1)
-  #   ) +
-  #   ggplot2::scale_colour_discrete(
-  #     name = "Probability on the island",
-  #     type = c("#046C9A", "#D69C4E", "#ECCBAE", "#ABDDDE", "#000000"))
-  #
-  #
-  # performance_prob_on_island <- performance_prob_on_island +
-  #   ggplot2::geom_point(
-  #     data = summary_empirical_results,
-  #     mapping = ggplot2::aes(
-  #       x = tree_size,
-  #       y = mean,
-  #       colour = tree_type),
-  #     size = 2
-  #   ) +
-  #   ggplot2::geom_errorbar(
-  #     data = summary_empirical_results,
-  #     mapping = ggplot2::aes(
-  #       x = tree_size,
-  #       y = mean,
-  #       ymin = mean-sd,
-  #       ymax = mean+sd,
-  #       colour = tree_type
-  #     ),
-  #     width = 0.3
-  #   )
-  #
-  # grouped_results <- dplyr::group_by(results, tree_size, prob_endemic)
-  # mean_results <- dplyr::summarise(
-  #   grouped_results,
-  #   mean = mean(time),
-  #   .groups = "drop"
-  # )
-  # sd_results <- dplyr::summarise(
-  #   grouped_results,
-  #   sd = sd(time),
-  #   .groups = "drop"
-  # )
-  #
-  # summary_results <- dplyr::right_join(
-  #   mean_results,
-  #   sd_results,
-  #   by = c("tree_size", "prob_endemic")
-  # )
-  #
-  # summary_results$prob_endemic <- as.factor(summary_results$prob_endemic)
-  #
-  # performance_prob_endemic <- ggplot2::ggplot(data = summary_results) +
-  #   ggplot2::geom_point(
-  #     mapping = ggplot2::aes(
-  #       x = tree_size,
-  #       y = mean,
-  #       colour = prob_endemic
-  #     ),
-  #     position = ggplot2::position_dodge(width = 0.5),
-  #     size = 2,
-  #   ) +
-  #   ggplot2::geom_errorbar(
-  #     mapping = ggplot2::aes(
-  #       x = tree_size,
-  #       y = mean,
-  #       ymin = pmax(0, mean-sd),
-  #       ymax = mean+sd,
-  #       colour = prob_endemic
-  #     ),
-  #     position = ggplot2::position_dodge(width = 0.5),
-  #     width = 0.3
-  #   ) +
-  #   ggplot2::theme_classic() +
-  #   ggplot2::scale_x_continuous(
-  #     name = "Tree Size (num. tips)",
-  #     trans = "log",
-  #     breaks = scales::breaks_log()
-  #   ) +
-  #   ggplot2::scale_y_continuous(
-  #     name = "Mean run time (seconds)",
-  #     trans = "log",
-  #     breaks = scales::breaks_log(),
-  #     labels = scales::comma_format(accuracy = 0.1)
-  #   ) +
-  #   ggplot2::scale_colour_discrete(
-  #     name = "Probability endemic",
-  #     type = c("#D8B70A", "#02401B"))
+  # store parameter estimates and parameters in tibble
+  performance_data <- tibble::as_tibble(performance_data)
 
+  grouped_performance_data <- dplyr::group_by(
+    performance_data,
+    tree_size,
+    {{ group_by }}
+  )
+
+  mean_performance_data <- dplyr::summarise(
+    grouped_performance_data,
+    mean = mean(median_time),
+    .groups = "drop"
+  )
+  sd_performance_data <- dplyr::summarise(
+    grouped_performance_data,
+    sd = sd(median_time),
+    .groups = "drop"
+  )
+
+  group_by_var <- rlang::set_names(rlang::as_string(rlang::ensym(group_by)))
+
+  summary_performance_data <- dplyr::right_join(
+    mean_performance_data,
+    sd_performance_data,
+    by = c("tree_size", group_by_var)
+  )
+
+  summary_performance_data <- dplyr::mutate(
+    summary_performance_data,
+    "{{ group_by }}" := factor({{ group_by }}),
+    .keep = "unused"
+  )
+
+  # create legend title depending on grouping variable
+  if (grepl(pattern = "island", x = group_by_var)) {
+    legend_title <- "Probability on the island"
+  } else {
+    legend_title <- "Probability endemic"
+  }
+
+  # create plot
+  performance_plot <- ggplot2::ggplot(summary_performance_data) +
+    ggplot2::geom_point(
+      mapping = ggplot2::aes(
+        x = tree_size,
+        y = mean,
+        colour = {{ group_by }}
+      ),
+      position = ggplot2::position_dodge(width = 0.5),
+      size = 2
+    ) +
+    ggplot2::geom_errorbar(
+      mapping = ggplot2::aes(
+        x = tree_size,
+        y = mean,
+        ymin = mean-sd,
+        ymax = mean+sd,
+        colour = {{ group_by }}
+      ),
+      position = ggplot2::position_dodge(width = 0.5),
+      width = 0.3
+    ) +
+    ggplot2::theme_classic() +
+    ggplot2::scale_x_continuous(
+      name = "Tree Size (number of tips)",
+      trans = "log",
+      breaks = scales::breaks_log()
+    ) +
+    ggplot2::scale_y_continuous(
+      name = "Mean run time (seconds)",
+      trans = "log",
+      breaks = scales::breaks_log()
+    ) +
+    ggplot2::scale_colour_discrete(
+      name = legend_title,
+      type = c("#046C9A", "#D69C4E", "#ECCBAE", "#ABDDDE", "#000000")
+    )
+
+  # return performance plot
+  performance_plot
 }
--- R/read_performance.R
+++ R/read_performance.R
@@ -1,13 +1,22 @@
+#' Reads in performance analysis results from inst/performance_data and formats
+#' the data ready for plotting
+#'
+#' @return List of two data frames
+#' @keywords internal
 read_performance <- function() {
   # load all the files from the path
   performance_data_files <- list.files(
-    path = system.file("performance_data", package = "DAISIEprepExtra")
+    path = system.file(
+      "performance_data",
+      package = "DAISIEprep",
+      mustWork = TRUE
+    )
   )
   if (length(performance_data_files) == 0) {
     stop("No results are in the results directory")
   } else {
     file_paths <- as.list(paste0(
-      system.file("performance_data", package = "DAISIEprepExtra"),
+      system.file("performance_data", package = "DAISIEprep", mustWork = TRUE),
       "/",
       performance_data_files
     ))
@@ -19,4 +28,96 @@ read_performance <- function() {
     performance_data <- lapply(file_paths, readRDS)
     performance_empirical <- readRDS(performance_empirical)
   }
+
+  # unpack list of results to merge tibbles
+  performance_data_min <- lapply(performance_data, "[[", "performance_min")
+  performance_data_asr <- lapply(performance_data, "[[", "performance_asr")
+
+  # unpack list of empirical results
+  performance_empirical_min_dna <- performance_empirical$times_min_dna
+  performance_empirical_min_complete <- performance_empirical$times_min_complete
+  performance_empirical_asr_dna <- performance_empirical$times_asr_dna
+  performance_empirical_asr_complete <- performance_empirical$times_asr_complete
+
+  # merge list of tibbles
+  performance_data_min <- Reduce(rbind, performance_data_min)
+  performance_data_asr <- Reduce(rbind, performance_data_asr)
+  performance_empirical_min_dna <- Reduce(rbind, performance_empirical_min_dna)
+  performance_empirical_min_complete <- Reduce(
+    rbind,
+    performance_empirical_min_complete
+  )
+  performance_empirical_asr_dna <- Reduce(rbind, performance_empirical_asr_dna)
+  performance_empirical_asr_complete <- Reduce(
+    rbind,
+    performance_empirical_asr_complete
+  )
+
+  # change prob_on_island to factor for simulated data
+  performance_data_min$prob_on_island <- factor(performance_data_min$prob_on_island)
+  performance_data_asr$prob_on_island <- factor(performance_data_asr$prob_on_island)
+
+  # change prob_endemic to factor for simulated data
+  performance_data_min$prob_endemic <- factor(performance_data_min$prob_endemic)
+  performance_data_asr$prob_endemic <- factor(performance_data_asr$prob_endemic)
+
+  # change prob_on_island to factor for empirical data
+  performance_empirical_min_dna$prob_on_island <- factor("DNA")
+  performance_empirical_min_complete$prob_on_island <- factor("complete")
+  performance_empirical_asr_dna$prob_on_island <- factor("DNA")
+  performance_empirical_asr_complete$prob_on_island <- factor("complete")
+
+  # change prob_endemic to factor for empirical data
+  performance_empirical_min_dna$prob_endemic <- factor("DNA")
+  performance_empirical_min_complete$prob_endemic <- factor("complete")
+  performance_empirical_asr_dna$prob_endemic <- factor("DNA")
+  performance_empirical_asr_complete$prob_endemic <- factor("complete")
+
+  # convert to tibble
+  performance_data_min <- tibble::as_tibble(performance_data_min)
+  performance_data_asr <- tibble::as_tibble(performance_data_asr)
+  performance_empirical_min_dna <- tibble::as_tibble(
+    performance_empirical_min_dna
+  )
+  performance_empirical_min_complete <- tibble::as_tibble(
+    performance_empirical_min_complete
+  )
+  performance_empirical_asr_dna <- tibble::as_tibble(
+    performance_empirical_asr_dna
+  )
+  performance_empirical_asr_complete <- tibble::as_tibble(
+    performance_empirical_asr_complete
+  )
+
+  # merge empirical data for min data
+  performance_empirical_min <- dplyr::bind_rows(
+    performance_empirical_min_dna,
+    performance_empirical_min_complete,
+    .id = "tree_type"
+  )
+
+  # merge empirical data for asr data
+  performance_empirical_asr <- dplyr::bind_rows(
+    performance_empirical_asr_dna,
+    performance_empirical_asr_complete,
+    .id = "tree_type"
+  )
+
+  # merge simulated and empirical data for min
+  performance_data_min <- dplyr::bind_rows(
+    performance_data_min,
+    performance_empirical_min
+  )
+
+  # merge simulated and empirical data for asr
+  performance_data_asr <- dplyr::bind_rows(
+    performance_data_asr,
+    performance_empirical_asr
+  )
+
+  # return list of sensitivity data for dna and complete
+  list(
+    performance_data_min = performance_data_min,
+    performance_data_asr = performance_data_asr
+  )
 }
