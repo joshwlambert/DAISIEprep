bf4dadbe2e2224261ca505efcc5a420c51b3340e
joshwlambert <j.w.l.lambert@rug.nl>
2022-07-26 10:02:10 +0200

    simplified extract_stem_age_genus() to look to stem of MRCA of genus to work with non-monophyly

--- R/extract_stem_age_genus.R
+++ R/extract_stem_age_genus.R
@@ -6,91 +6,20 @@
 #'
 #' @return Numeric
 extract_stem_age_genus <- function(genus_in_tree,
-                                   phylod,
-                                   constrain_to_island) {
+                                   phylod) {
 
-  extracted_col_times <- c()
+  # set ingroup of species
+  ingroup <- genus_in_tree
 
-  # get the genus names to be includes in the clade to extract stem age
-  genera <- phylobase::tipLabels(phylod)[genus_in_tree]
-  genera <- strsplit(x = genera, split = "_")
-  genera <- sapply(genera, "[[", 1)
-  # add for loop to loop over genus_in_tree elements
-  for (i in genus_in_tree) {
-    species_label <- phylobase::tipLabels(phylod)[i]
+  # get the crown age of the ingroup
+  crown <- phylobase::MRCA(tree, ingroup)
 
-    if (constrain_to_island) {
-      species_endemicity <-
-        phylobase::tdata(phylod)[genus_in_tree, "endemicity_status"]
-      if (all(species_endemicity == "not_present")) {
-        stop("No species in the genus on the island cannot constain to island")
-      }
-    }
+  # the stem age is the parent of the crown node
+  stem <- phylobase::ancestor(phy = tree, node = crown)
 
-    # set up variables to be modified in the loop
-    all_siblings <- TRUE
-    ancestor <- species_label
-    descendants <- 1
-    names(descendants) <- species_label
-
-    # recursive tree traversal to find endemic clade
-    while (all_siblings) {
-      ancestor <- phylobase::ancestor(phy = phylod, node = ancestor)
-      # save a copy of descendants for when loop stops
-      clade <- descendants
-      descendants <- phylobase::descendants(phy = phylod, node = ancestor)
-      # get endemicity of siblings
-      which_siblings <- which(phylobase::labels(phylod) %in% names(descendants))
-      # check whether all siblings are in the focal genera when not constraining
-      # to the island species within the genus
-      sibling_genus <- unname(phylobase::tipLabels(phylod))[which_siblings]
-      split_species_names <- strsplit(x = sibling_genus, split = "_")
-      genus_names <- sapply(split_species_names, "[[", 1)
-      all_siblings <- all(genus_names %in% genera)
-      if (constrain_to_island) {
-        # check whether all siblings are on the island when constraining to
-        # island clade within the genus
-        sibling_endemicity <-
-          phylobase::tdata(phylod)[which_siblings, "endemicity_status"]
-        all_island_siblings <- all(
-          sibling_endemicity %in% c("endemic", "nonendemic")
-        )
-        # siblings must be of the same genus and on island
-        all_siblings <- all_siblings && all_island_siblings
-      }
-    }
-
-    if (length(clade) == 1) {
-      # extract stem age
-      col_time <- as.numeric(phylobase::edgeLength(phylod, names(clade)))
-
-      # add stem age
-      extracted_col_times <- c(extracted_col_times, col_time)
-    } else {
-      # use S3 phylo objects for speed
-      # suppress warnings about tree conversion as they are fine
-      phylo <- suppressWarnings(methods::as(phylod, "phylo"))
-
-      # extract colonisation time as stem age of clade (time before present)
-      mrca <- ape::getMRCA(phylo, tip = clade)
-      stem <- phylo$edge[which(phylo$edge[, 2] == mrca), 1]
-      col_times <- ape::node.depth.edgelength(phy = phylo)
-
-      # convert from distance from root to distance from tip
-      col_times <- abs(col_times - max(col_times))
-
-      # get only the stem age
-      col_time <- col_times[stem]
-
-      # add stem age
-      extracted_col_times <- c(extracted_col_times, col_time)
-    }
-
-  }
-
-  # maximum age from those extracted
-  col_time <- max(extracted_col_times)
+  # get time of stem as time before present (assuming ultrametric tree)
+  stem <- unname(phylobase::nodeHeight(x = tree, node = stem, from = "min_tip"))
 
   # return stem age
-  col_time
+  stem
 }
